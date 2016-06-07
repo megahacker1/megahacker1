ebe0905f1d95e35273f3321c56c415d868fd2789
mohamadtip3i1 <mohamadtip3i_mohamadtip3i@yahoo.com>
2016-06-07 22:59:32 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,13 +226,13 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {228065368(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
+https://github.com/megahacker1/megahacker1
 
 Our team!
 Alphonse (@Iwals)
